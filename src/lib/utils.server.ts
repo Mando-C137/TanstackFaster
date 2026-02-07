5a0e5f95c6b7c803cada94adee9ac92c10d14855
Mando-C137 <paulheisner@gmail.com>
2026-02-07 15:24:37 +0100

    fix: og

--- src/lib/utils.server.ts
+++ src/lib/utils.server.ts
@@ -1,9 +1,12 @@
-import fs from "node:fs/promises";
-import path from "node:path";
 import { createServerOnlyFn } from "@tanstack/react-start";
 
 export const loadFont = createServerOnlyFn(
-  (name: "geist-sans-latin-400-normal.ttf") => {
-    return fs.readFile(path.join(process.cwd(), "src/assets/fonts", name));
+  async (name: "geist-sans-latin-400-normal.ttf") => {
+    const baseUrl = process.env.VERCEL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000";
+
+    const r = await fetch(`${baseUrl}/fonts/${name}`);
+    return await r.arrayBuffer();
   },
 );
