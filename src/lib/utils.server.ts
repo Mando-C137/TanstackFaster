4b7b05a6a33b53983c1203f0770cab9362af8ae0
Mando-C137 <paulheisner@gmail.com>
2026-02-08 16:21:09 +0100

    chore: ran prettier and updated deps [skip ci]

--- src/lib/utils.server.ts
+++ src/lib/utils.server.ts
@@ -3,7 +3,7 @@ import { GEIST_FONT_BASE64 } from "./font-data";
 
 export const loadFont = createServerOnlyFn(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async (_name: "geist-sans-latin-400-normal.ttf") => {
+  (_name: "geist-sans-latin-400-normal.ttf") => {
     // Use embedded base64 font - 100% reliable, no file system or network needed
     const binaryString = atob(GEIST_FONT_BASE64);
     const bytes = new Uint8Array(binaryString.length);
